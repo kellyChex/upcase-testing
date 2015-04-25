a565ea092d973a27e171bbbeaeb4ff3801972b9a
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-04-25 07:20:34 +0100

    Extract Value Object

---
+++ extract-value-object/lib/report_card.rb
@@ -0,0 +1,20 @@
+class ReportCard
+  attr_accessor :grades
+
+  def initialize(attributes = {})
+    @scores = attributes[:scores]
+    @grades ||= grade_scores
+  end
+
+  private
+
+  def grade_scores
+    @scores.map do |score|
+      grade_score(score)
+    end
+  end
+
+  def grade_score(score)
+    Grade.result(score: score)
+  end
+end
