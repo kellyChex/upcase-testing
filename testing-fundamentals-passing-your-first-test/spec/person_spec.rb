c27541a6fee9b0d0c75857c250f003a605dfce50
KellyChess <chess.kelly@gmail.com>
2015-12-06 15:41:29 -0800

    first commit

--- testing-fundamentals-passing-your-first-test/spec/person_spec.rb
+++ testing-fundamentals-passing-your-first-test/spec/person_spec.rb
@@ -3,7 +3,6 @@ require_relative "../lib/person"
 describe Person do
   describe "#full_name" do
     it "returns the first and last names concatenated" do
-      # Write your spec here.
       person = Person.new first_name: 'kingsley', last_name: 'ijomah'
       expect(person.full_name).to eq('kingsley ijomah')
     end
